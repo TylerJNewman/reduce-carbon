6dc490dc2746e15f6d2a93965b03ecc9fdcd8590
Tyler Newman <tnewman@clairglobal.com>
2021-02-28 09:24:05 -0800

    Fix mobile

--- pages/index.js
+++ pages/index.js
@@ -13,14 +13,14 @@ export default function Index() {
         <nav className="">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="flex items-center justify-between h-24">
-              <div className="flex items-center">
+              <div className=" flex items-center">
                 <img
                   className="h-12 w-12"
                   src="/android-chrome-512x512.png"
                   alt="Logo"
                 />
               </div>
-              <div className="hidden md:block">
+              <div className=" hidden md:block">
                 <div className="ml-10 flex items-baseline space-x-4">
                   <a
                     href="#"
@@ -58,7 +58,7 @@ export default function Index() {
                 </div>
               </div>
 
-              <div className="-mr-2 flex md:hidden">
+              <div className=" flex md:hidden">
                 {/* Mobile menu button */}
                 <button
                   onClick={toggleMobileMenu}
@@ -117,7 +117,7 @@ export default function Index() {
             className={`${menuOpen ? 'block' : 'hidden'} md:hidden`}
             id="mobile-menu"
           >
-            <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+            <div className="px-4 pt-2 pb-3 space-y-1 sm:px-3">
               <a
                 href="#"
                 className="bg-green-800 text-white block px-3 py-2 rounded-md text-base font-medium"
@@ -155,15 +155,24 @@ export default function Index() {
           <h1 className="center text-3xl font-bold text-white">Dashboard</h1>
         </div> */}
         <main>
-          <div className="section max-w-7xl mx-auto sm:px-6 lg:px-8 flex items-center justify-center h-screen">
+          <div className="section max-w-7xl mx-auto sm:px-6 lg:px-8 flex items-center justify-center">
             <div className="content">
-              <div className="py-6 flex justify-center">
-                <div className="w-96 h-96 flex justify-center">
-                  <img
-                    style={{width: '60%', height: '60%', objectFit: 'cover'}}
-                    src="/android-chrome-512x512.png"
-                    alt="Logo"
-                  />
+              <div
+                className="py-6 flex justify-center"
+                style={{maxWidth: '66%', margin: 'auto'}}
+              >
+                <div className="flex justify-center">
+                  <div className="max-w-md">
+                    <img
+                      style={{
+                        width: '100%',
+                        height: '100%',
+                        objectFit: 'cover',
+                      }}
+                      src="/android-chrome-512x512.png"
+                      alt="Logo"
+                    />
+                  </div>
                 </div>
               </div>
               <div className="space-y-4 py-4 text-center px-6">
@@ -174,7 +183,7 @@ export default function Index() {
               <div className="h-72"></div>
             </div>
           </div>
-          <div className="section max-w-7xl mx-auto sm:px-6 lg:px-8 flex items-center justify-center h-screen">
+          <div className="section max-w-7xl mx-auto sm:px-6 lg:px-8 flex items-center justify-center">
             <div className="content" style={{paddingBottom: '200px'}}>
               <div className="space-y-4 py-4 text-center">
                 <h1

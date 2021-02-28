810d764a607d153a5d284d141ec0eb1f0c9c8f94
Tyler Newman <tnewman@clairglobal.com>
2021-02-27 22:46:04 -0800

    First Draft of style and layout

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,226 @@
-import Head from 'next/head'
+// import Head from 'next/head'
+import React from 'react'
+import Navbar from '../src/components/navbar'
+// import Home from '../src/components/home'
 
-export default function Home() {
+export default function Index() {
+  const [menuOpen, setMenuOpen] = React.useState(false)
+  const toggleMobileMenu = () => setMenuOpen(!menuOpen)
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
+      <div>
+        <nav className="">
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="flex items-center justify-between h-24">
+              <div className="flex items-center">
+                <img
+                  className="h-12 w-12"
+                  src="/android-chrome-512x512.png"
+                  alt="Logo"
+                />
+              </div>
+              <div className="hidden md:block">
+                <div className="ml-10 flex items-baseline space-x-4">
+                  <a
+                    href="#"
+                    className="text-white  hover:text-green-700 px-3 py-2 rounded-md text-md font-medium"
+                  >
+                    About
+                  </a>
+                  <a
+                    href="#"
+                    className="text-white  hover:text-green-700 px-3 py-2 rounded-md text-md font-medium"
+                  >
+                    What
+                  </a>
+                  <a
+                    href="#"
+                    className="text-white  hover:text-green-700 px-3 py-2 rounded-md text-md font-medium"
+                  >
+                    Why
+                  </a>
+                  <a
+                    href="#"
+                    className="text-white  hover:text-green-700 px-3 py-2 rounded-md text-md font-medium"
+                  >
+                    How
+                  </a>
+                  {/* <button
+                    href="#"
+                    className="text-white  hover:bg-green-900 px-3 py-2 rounded-md text-md font-medium"
+                  >
+                    Contact
+                  </button> */}
+                  <button className="bg-transparent hover:bg-green-600  font-semibold text-white py-2 px-4 border border-white hover:border-transparent rounded">
+                    Contact
+                  </button>
+                </div>
+              </div>
 
-      <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+              <div className="-mr-2 flex md:hidden">
+                {/* Mobile menu button */}
+                <button
+                  onClick={toggleMobileMenu}
+                  type="button"
+                  className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                  aria-controls="mobile-menu"
+                  aria-expanded="false"
+                >
+                  <span className="sr-only">Open main menu</span>
+                  {/*
+              Heroicon name: outline/menu
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
+              Menu open: "hidden", Menu closed: "block"
+            */}
+                  <svg
+                    className={`${menuOpen ? 'hidden' : 'block'} h-6 w-6`}
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                  {/*
+              Heroicon name: outline/x
 
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+              Menu open: "block", Menu closed: "hidden"
+            */}
+                  <svg
+                    className={`${menuOpen ? 'block' : 'hidden'} h-6 w-6`}
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </div>
+          {/* Mobile menu, show/hide based on menu state. */}
+          <div
+            className={`${menuOpen ? 'block' : 'hidden'} md:hidden`}
+            id="mobile-menu"
           >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
-    </div>
+            <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+              <a
+                href="#"
+                className="bg-green-800 text-white block px-3 py-2 rounded-md text-base font-medium"
+              >
+                About
+              </a>
+              <a
+                href="#"
+                className="text-white hover:bg-green-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+              >
+                What
+              </a>
+              <a
+                href="#"
+                className="text-white hover:bg-green-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+              >
+                Why
+              </a>
+              <a
+                href="#"
+                className="text-white hover:bg-green-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+              >
+                How
+              </a>
+              <a
+                href="#"
+                className="text-white hover:bg-green-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+              >
+                Contact
+              </a>
+            </div>
+          </div>
+        </nav>
+        {/* <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+          <h1 className="center text-3xl font-bold text-white">Dashboard</h1>
+        </div> */}
+        <main>
+          <div className="section max-w-7xl mx-auto sm:px-6 lg:px-8 flex items-center justify-center h-screen">
+            <div className="content">
+              <div className="py-6 flex justify-center">
+                <img
+                  className="h-96 w-96"
+                  src="/android-chrome-512x512.png"
+                  alt="Logo"
+                />
+              </div>
+              <div className="space-y-4 py-4 text-center px-6">
+                <h4 className="block text-white font-bold">
+                  Funneling the power of the meme to help the world
+                </h4>
+              </div>
+              <div className="h-72"></div>
+            </div>
+          </div>
+          <div className="section max-w-7xl mx-auto sm:px-6 lg:px-8 flex items-center justify-center h-screen">
+            <div className="content" style={{paddingBottom: '200px'}}>
+              {/* <div className="py-6 flex justify-center">
+                <img
+                  className="h-96 w-96"
+                  src="/android-chrome-512x512.png"
+                  alt="Logo"
+                />
+              </div> */}
+              <div className="space-y-4 py-4 text-center">
+                <h1
+                  className="block text-white font-bold py-24"
+                  styles="margin-top: -170px"
+                >
+                  About
+                </h1>
+              </div>
+              <div className="container px-6 mx-auto flex flex-wrap flex-col md:flex-row items-center">
+                <div className="flex flex-col w-full xl:w-2/5 justify-center lg:items-start overflow-y-hidden">
+                  <h5 className="my-4  md:text-2xl text-white font-bold leading-tight text-center md:text-left slide-in-bottom-h1">
+                    Reduce Carbon Club turns carbon into diamonds and profits
+                    while helping the environment.
+                  </h5>
+                  <p className="text-white leading-normal text-base md:text-2xl mb-8 text-center md:text-left slide-in-bottom-subtitle">
+                    Saving the world is hard. The size and scale of the climate
+                    crisis demands a level of collaboration that was never
+                    before possible. 
+                  </p>
+                  <p className="italic text-white leading-normal text-base md:text-2xl mb-8 text-center md:text-left slide-in-bottom-subtitle">
+                    "We can save Earth...It's going to take collective action
+                    from big companies, small companies, nation-states, global
+                    organizations, and individuals."
+                  </p>
+                  <p className=" text-white leading-normal text-base md:text-2xl mb-8 text-center md:text-left slide-in-bottom-subtitle">
+                    - Jeff Bezos, February 17th, 2021
+                  </p>
+                </div>
+                {/*Right Col*/}
+                <div className="w-full xl:w-3/5 py-6 overflow-y-hidden">
+                  <img
+                    className="w-5/6 mx-auto lg:mr-0 slide-in-bottom max-w-prose"
+                    src="/diamond.png"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
+      </div>
+    </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,103 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['avenir', ...defaultTheme.fontFamily.sans],
+      serif: [...defaultTheme.fontFamily.serif],
+      mono: [...defaultTheme.fontFamily.mono],
+    },
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: '#53b29e',
+        // red: {
+        //   // light: '#85d7ff',
+        //   DEFAULT: '#CC3860',
+        //   // dark: '#009eeb',
+        // },
+        cream: {
+          DEFAULT: '#e0e0e0',
+        },
+        blue: {
+          dark: '#0268C3',
+        },
+
+        green: {
+          100: '#ddf0ec',
+          200: '#bae0d8',
+          300: '#98d1c5',
+          400: '#75c1b1',
+          500: '#53b29e',
+          600: '#428e7e',
+          700: '#326b5f',
+          800: '#21473f',
+          900: '#112420',
+        },
+
+        indigo: {
+          100: '#cce1f3',
+          200: '#9ac3e7',
+          300: '#67a4db',
+          400: '#3586cf',
+          500: '#0268c3',
+          600: '#02539c',
+          700: '#013e75',
+          800: '#012a4e',
+          900: '#001527',
+        },
+
+        pink: {
+          100: '#f8e0e6',
+          200: '#f1c0cd',
+          300: '#e9a1b4',
+          400: '#e2819b',
+          500: '#db6282',
+          600: '#af4e68',
+          700: '#833b4e',
+          800: '#582734',
+          900: '#2c141a',
+        },
+
+        orange: {
+          100: '#fdeae4',
+          200: '#fcd6c9',
+          300: '#fac1ae',
+          400: '#f9ad93',
+          500: '#f79878',
+          600: '#c67a60',
+          700: '#945b48',
+          800: '#633d30',
+          900: '#311e18',
+        },
+
+        // pink: {
+        //   100: '#f5d7df',
+        //   200: '#ebafbf',
+        //   300: '#e088a0',
+        //   400: '#d66080',
+        //   500: '#cc3860',
+        //   600: '#a32d4d',
+        //   700: '#7a223a',
+        //   800: '#521626',
+        //   900: '#290b13',
+        // },
+        // pink: {
+        //   light: '#ff7ce5',
+        //   DEFAULT: '#ff49db',
+        //   dark: '#ff16d1',
+        // },
+        // gray: {
+        //   darkest: '#1f2d3d',
+        //   dark: '#3c4858',
+        //   DEFAULT: '#c0ccda',
+        //   light: '#e0e6ed',
+        //   lightest: '#f9fafc',
+        // },
+      },
+    },
   },
   variants: {
     extend: {},

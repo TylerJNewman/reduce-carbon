f5e43c6a86472788e39b4e25fd4dc117ce04f546
Tyler Newman <tnewman@clairglobal.com>
2021-02-28 22:29:07 -0800

    Add Ecosytem

---
+++ components/Features.js
@@ -0,0 +1,151 @@
+import React from 'react'
+import Section from './Section'
+import {useScroll} from './scroll-context'
+
+const Features = () => {
+  const {featuresRef} = useScroll()
+  return (
+    <>
+      <Section forwardedRef={featuresRef}>
+        <div className="py-4  mx-auto sm:px-6 lg:px-8">
+          <h3 className="block text-center text-white font-bold py-8 md:pb-24">
+            Ecosystem
+          </h3>
+          <div className="py-12">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+              <div className="mt-10">
+                <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
+                  <div className="flex">
+                    <div className="flex-shrink-0">
+                      <div className="flex items-center justify-center h-12 w-12 rounded-md bg-green-600 text-white">
+                        {/* Heroicon name: outline/globe-alt */}
+                        <svg
+                          className="h-6 w-6"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          aria-hidden="true"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"
+                          />
+                        </svg>
+                      </div>
+                    </div>
+                    <div className="ml-4">
+                      <dt className="text-lg leading-6 font-medium text-white">
+                        Decentralized
+                      </dt>
+                      <dd className="mt-2 text-base text-white">
+                        💎🤲 DAO is fully decentralised, there is no one main
+                        owner of the token. All decisions are made in the
+                        communities favor.
+                      </dd>
+                    </div>
+                  </div>
+                  <div className="flex">
+                    <div className="flex-shrink-0">
+                      <div className="flex items-center justify-center h-12 w-12 rounded-md bg-green-600 text-white">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          stroke="currentColor"
+                          stroke-width="2"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          class="feather feather-droplet"
+                        >
+                          <path d="M12 2.69l5.66 5.66a8 8 0 1 1-11.31 0z"></path>
+                        </svg>
+                      </div>
+                    </div>
+                    <div className="ml-4">
+                      <dt className="text-lg leading-6 font-medium text-white">
+                        Burn Rate
+                      </dt>
+                      <dd className="mt-2 text-base text-white">
+                        Each transaction triggers a burn rate of 1%, which
+                        decreases the supply of DMND overtime.
+                      </dd>
+                    </div>
+                  </div>
+                  <div className="flex">
+                    <div className="flex-shrink-0">
+                      <div className="flex items-center justify-center h-12 w-12 rounded-md bg-green-600 text-white">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          stroke="currentColor"
+                          stroke-width="2"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          class="feather feather-box"
+                        >
+                          <path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"></path>
+                          <polyline points="3.27 6.96 12 12.01 20.73 6.96"></polyline>
+                          <line x1="12" y1="22.08" x2="12" y2="12"></line>
+                        </svg>
+                      </div>
+                    </div>
+                    <div className="ml-4">
+                      <dt className="text-lg leading-6 font-medium text-white">
+                        Rewards
+                      </dt>
+                      <dd className="mt-2 text-base text-white">
+                        💎🤲 DAO rewards its holders with a 1% tax on each
+                        transaction to punish weak hands.
+                      </dd>
+                    </div>
+                  </div>
+                  <div className="flex">
+                    <div className="flex-shrink-0">
+                      <div className="flex items-center justify-center h-12 w-12 rounded-md bg-green-600 text-white">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          stroke="currentColor"
+                          stroke-width="2"
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          class="feather feather-edit"
+                        >
+                          <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
+                          <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
+                        </svg>
+                      </div>
+                    </div>
+                    <div className="ml-4">
+                      <dt className="text-lg leading-6 font-medium text-white">
+                        Further Development
+                      </dt>
+                      <dd className="mt-2 text-base text-white">
+                        💎🤲 DAO is planned to advance further into the crypto
+                        space by releasing innovative products which will ensure
+                        the sustainability of the token.
+                      </dd>
+                    </div>
+                  </div>
+                </dl>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Section>
+    </>
+  )
+}
+
+export default Features
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,8 @@ import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import About from '../components/About'
 import How from '../components/How'
+import Features from '../components/Features'
+// import Contact from '../components/Contact'
 import {ScrollProvider} from '../components/scroll-context'
 
 export default function Index() {
@@ -14,6 +16,8 @@ export default function Index() {
         <Hero />
         <About />
         <How />
+        <Features />
+        {/* <Contact /> */}
       </main>
     </ScrollProvider>
   )

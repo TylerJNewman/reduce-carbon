4161abda95821e4dc2d591b2a3f1e8d4d1fa7a2d
Tyler Newman <tnewman@clairglobal.com>
2021-03-03 22:49:08 -0800

    Try react-spring animation

--- components/Hero.js
+++ components/Hero.js
@@ -1,8 +1,21 @@
 import React from 'react'
 import Section from './Section'
 import {useScroll} from './scroll-context'
+import {useSpring, animated} from 'react-spring'
+
+const calc = (x, y) => [
+  -(y - window.innerHeight / 2) / 20,
+  (x - window.innerWidth / 2) / 20,
+  1.1,
+]
+const trans = (x, y, s) =>
+  `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
 
 const Hero = () => {
+  const [props, set] = useSpring(() => ({
+    xys: [0, 0, 1],
+    config: {mass: 5, tension: 350, friction: 40},
+  }))
   return (
     <Section name="hero">
       <div className="content">
@@ -11,7 +24,17 @@ const Hero = () => {
           style={{maxWidth: '66%', margin: 'auto'}}
         >
           <div className="flex justify-center">
-            <div className="max-w-md">
+            <div className="max-w-md hidden md:block">
+              <animated.div
+                class="card"
+                onMouseMove={({clientX: x, clientY: y}) =>
+                  set({xys: calc(x, y)})
+                }
+                onMouseLeave={() => set({xys: [0, 0, 1]})}
+                style={{transform: props.xys.interpolate(trans)}}
+              ></animated.div>
+            </div>
+            <div className="max-w-md block md:hidden">
               <img
                 style={{
                   width: '100%',
@@ -24,7 +47,8 @@ const Hero = () => {
             </div>
           </div>
         </div>
-        <div className="space-y-4 py-4 text-center px-6 md:mt-8">
+
+        <div className="space-y-4 py-8 text-center px-6 md:mt-16">
           <h4 className="block text-white font-bold">
             Funneling the power of the meme to help the world
           </h4>
